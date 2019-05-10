c6036c0895adc1636d1ff32413b573227420e9ae
Mark Aleksander Hil <mark@100shapes.com>
2019-05-10 13:48:51 +0100

    Added Dockerfile

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index:
+def index():
     return "Index page"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
