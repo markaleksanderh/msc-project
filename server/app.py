347fa9016bdabdd84b5140a1a3fe299eb03dd681
Mark Aleksander Hil <mark@100shapes.com>
2019-05-14 16:51:28 +0100

    Adding neo4j to docker-compose

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index page"
+    return jsonify({'message': "Hello World"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
