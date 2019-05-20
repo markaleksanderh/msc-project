ebea9397854bbeaf42d7b87ae5f9a5d3d7c008ff
Mark Aleksander Hil <mark@100shapes.com>
2019-05-20 12:40:18 +0100

    Refactored to use app factory pattern

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, jsonify
-from flask_graphql import GraphQLView
-from .schemas import *
+# from flask import Flask, jsonify
+# from flask_graphql import GraphQLView
+# from .schemas import *
 
-app = Flask(__name__)
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=models, graphiql=True))
+# app = Flask(__name__)
+# app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=models, graphiql=True))
 
 
-@app.route('/')
-def index():
-    return jsonify({'message': "Hello World"})
+# @app.route('/')
+# def index():
+#     return jsonify({'message': "Hello World"})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
 
