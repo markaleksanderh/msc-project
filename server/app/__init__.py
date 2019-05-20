f7ae4e8c140864de858fa83b35120023b01699a2
Mark Aleksander Hil <mark@100shapes.com>
2019-05-20 12:42:42 +0100

    Add 404 route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+# from flask_graphql import GraphQLView
 
 def create_app():
     app = Flask(__name__)
+
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return "404 Page not found"
     return app
