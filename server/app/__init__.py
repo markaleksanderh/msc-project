efd5cd26d9c07c2d75d9b7171e06c4a9f35e480c
Mark Aleksander Hil <mark@100shapes.com>
2019-05-20 14:23:58 +0100

    Working /graphql endpoint but requirements.txt doesn't install

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
-# from flask_graphql import GraphQLView
+from flask_graphql import GraphQLView
+from .schemas import schema
+
 
 def create_app():
     app = Flask(__name__)
-
+    app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
+        'graphql', schema=schema, graphiql=True
+    ))
 
     @app.errorhandler(404)
     def page_not_found(error):
