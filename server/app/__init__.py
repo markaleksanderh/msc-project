868252be3141527c49cc3cf8595a883264b883bc
Mark Aleksander Hil <mark@100shapes.com>
2019-05-21 14:24:54 +0100

    Added temporary index page route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,6 +5,11 @@ from .schemas import schema
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return "Index page"
+
     app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
         'graphql', schema=schema, graphiql=True
     ))
