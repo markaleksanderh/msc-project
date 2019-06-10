e79b0e91e23314446bfaee9eb376cfc9f5839c9e
Mark Aleksander Hil <mark@100shapes.com>
2019-06-10 16:55:56 +0100

    Updated models

--- server/app/models.py
+++ server/app/models.py
@@ -1,37 +1,43 @@
 from graphql import GraphQLError
-from py2neo import Graph, Node
+from py2neo import Graph, Node, Relationship
 from py2neo.ogm import GraphObject, Property, RelatedTo
 
-
 # Set up config file
-graph = Graph(
-    # bolt=True,
-    host='localhost',
-    port=7687,
-    user='neo4j',
-    password='letmein',
-)
+# graph = Graph(
+#     # bolt=True,
+#     host='localhost',
+#     port=7687,
+# )
 
-# graph = Graph("bolt://localhost:7687")
+graph = Graph("bolt://localhost:7687")
 
 
-class BaseModel(GraphObject):
-    def __init__(self, **kwargs):
-        for key, value, in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+class Project(GraphObject):
+    def __init__(self, name):
+        self.name = name
+    __primarykey__ = "name"
+    name = Property()
 
-    @property
-    def all(self):
-        return self.select(graph)
-    
     def save(self):
         graph.push(self)
 
-class Project(BaseModel):
-    __primarykey__ = 'name'
-    name = Property()
+# class BaseModel(GraphObject):
+#     def __init__(self, **kwargs):
+#         for key, value, in kwargs.items():
+#             if hasattr(self, key):
+#                 setattr(self, key, value)
 
-    def fetch(self):
-        return self.select(graph, self.name).first()
+#     @property
+#     def all(self):
+#         return self.select(graph)
+    
+#     def save(self):
+#         graph.push(self)
+
+# class Project(BaseModel):
+#     __primarykey__ = 'name'
+#     name = Property()
+
+#     def fetch(self):
+#         return self.select(graph, self.name).first()
 
