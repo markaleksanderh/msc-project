ac7c7387f32e67120d9fae893b59d2fcd68e6bfe
Mark Aleksander Hil <mark@100shapes.com>
2019-06-13 13:01:04 +0100

    Commented mutation, query all

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -7,25 +7,38 @@ class ProjectSchema(graphene.ObjectType):
     name = graphene.String()
 
     
-class CreateProject(graphene.Mutation):
-    class Arguments:
-        name = graphene.String(required=True)
+# class CreateProject(graphene.Mutation):
+#     class Arguments:
+#         name = graphene.String(required=True)
 
-    success = graphene.Boolean()
-    project = graphene.Field(lambda: ProjectSchema)
+#     success = graphene.Boolean()
+#     project = graphene.Field(lambda: ProjectSchema)
 
-    def mutate(self, info,**kwargs):
-        project = Project(**kwargs)
-        project.save()
-        return CreateProject(project=project, success=True)
+#     def mutate(self, info,**kwargs):
+#         project = Project(**kwargs)
+#         project.save()
+#         return CreateProject(project=project, success=True)
 
 # Query
+# class Query(graphene.ObjectType):
+
+#     projects = graphene.List(lambda: ProjectSchema, name=graphene.String())
+
 class Query(graphene.ObjectType):
+    # project = graphene.Field(lambda: ProjectSchema, name=graphene.String())
+   
+    # def resolve_project(self, info, name):
+    #     project = Project(name=name).fetch()
+    #     return ProjectSchema(**project.as_dict())
     projects = graphene.List(lambda: ProjectSchema)
 
+    def resolve_projects(self, info):
+        [ProjectSchema(**project.as_dict()) for project in Project().all]
+
 # Mutations
-class Mutations(graphene.ObjectType):
-    create_project = CreateProject.Field()
+# class Mutations(graphene.ObjectType):
+#     create_project = CreateProject.Field()
 
 
-schema = graphene.Schema(query=Query, mutation=Mutations, auto_camelcase=False)
+# schema = graphene.Schema(query=Query, mutation=Mutations, auto_camelcase=False)
+schema = graphene.Schema(query=Query, auto_camelcase=False)
